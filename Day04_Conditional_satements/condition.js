0494c1d6e683d59ce33fd20e4677264bc0789ae8
Jaydeepwakade <jaydeepwakade123@gmail.com>
2024-03-01 00:24:37 +0530

    BMi calculator Ui and some logical code done

--- Day04_Conditional_satements/condition.js
+++ Day04_Conditional_satements/condition.js
@@ -1,117 +1,130 @@
-// Declare two variables and calculate their sum, product, and remainder.gti
+// // Declare two variables and calculate their sum, product, and remainder.gti
 
- let a = 20;
- let b = 7;
+//  let a = 20;
+//  let b = 7;
 
- console.log(a*b);
- console.log(a+b);
- console.log(a%b);
-
-
-// Compare the lengths of two strings and log whether they are equal or not.
-
-let string1 = "javascript";
-
-let string2 = "python";
-
-  if (str1 === str2){
-    console.log("equal");
-  }
-  else{
-    console.log("not");
-  }
-
-// Declare three boolean variables and use logical operators to determine if a given combination is true or false.
-let isSunny = true;
-let isWarm = false;
-let isWeekend = true;
+//  console.log(a*b);
+//  console.log(a+b);
+//  console.log(a%b);
 
 
-if (isSunny && isWarm || isWeekend){
-    console.log("it is a good day");
-}
-else{
-    console.log("it is a boring day ")
-}
+// // Compare the lengths of two strings and log whether they are equal or not.
 
-// Use if-else statement to determine if a number is positive, negative, or zero.
-let number = -20;
-  if( number < 0 ){
-    console.log("minus")
-  }
-  else if (number > 0){
-    console.log(" positive")
-  }
-  else{
-    console.log("Zero")
-  }
+// let string1 = "javascript";
 
+// let string2 = "python";
 
-// Use the ternary operator to check if a person is eligible to vote based on their age.
-let age = 6;
+//   if (str1 === str2){
+//     console.log("equal");
+//   }
+//   else{
+//     console.log("not");
+//   }
 
-  age > 18 ? console.log("able to vote") : console.log("you are child");
- let eligibility = age>=18 ? "you are able to vote" : "you are not able to vote as you'r age is not enough"
-console.log(eligibility)
+// // Declare three boolean variables and use logical operators to determine if a given combination is true or false.
+// let isSunny = true;
+// let isWarm = false;
+// let isWeekend = true;
 
 
-// Declare two strings and concatenate them. Check if the concatenated string is equal to "HelloWorld".
+// if (isSunny && isWarm || isWeekend){
+//     console.log("it is a good day");
+// }
+// else{
+//     console.log("it is a boring day ")
+// }
 
-let str1 = "Hello";
-let str2 = "World";
+// // Use if-else statement to determine if a number is positive, negative, or zero.
+// let number = -20;
+//   if( number < 0 ){
+//     console.log("minus")
+//   }
+//   else if (number > 0){
+//     console.log(" positive")
+//   }
+//   else{
+//     console.log("Zero")
+//   }
 
-let str3 = str1+str2
-let hello="HelloWorld"
 
-if(str3 === hello){
-    console.log(str3);
-}
-else{
-    console.log("kela ghya");
-}
+// // Use the ternary operator to check if a person is eligible to vote based on their age.
+// let age = 6;
 
-// // Declare three numbers and use logical operators to check if they form an increasing sequence.
-let num1 = 5;
-let num2 = 10;
-let num3 = 15;
+//   age > 18 ? console.log("able to vote") : console.log("you are child");
+//  let eligibility = age>=18 ? "you are able to vote" : "you are not able to vote as you'r age is not enough"
+// console.log(eligibility)
 
-let sequence = num1 < num2 && num2 < num3
-    console.log(sequence)
 
-//if else else if statement marks  and grade example
+// // Declare two strings and concatenate them. Check if the concatenated string is equal to "HelloWorld".
 
-marks =25;
+// let str1 = "Hello";
+// let str2 = "World";
 
-if (marks > 90 && marks < 90){
-    console.log("AA");
-  }
+// let str3 = str1+str2
+// let hello="HelloWorld"
+
+// if(str3 === hello){
+//     console.log(str3);
+// }
+// else{
+//     console.log("kela ghya");
+// }
+
+// // // Declare three numbers and use logical operators to check if they form an increasing sequence.
+// let num1 = 5;
+// let num2 = 10;
+// let num3 = 15;
+
+// let sequence = num1 < num2 && num2 < num3
+//     console.log(sequence)
+
+// //if else else if statement marks  and grade example
+
+// marks =25;
+
+// if (marks > 90 && marks < 90){
+//     console.log("AA");
+//   }
  
- else if (marks > 80 && marks <= 90){
-     console.log("AB");
- }
-  else if (marks > 70 && marks <= 80){
-        console.log("BB");
-  }
- else if( marks > 60 && marks <= 70){
-         console.log("BC");
- }
- else if( marks > 50 && marks <= 60){
-      console.log("CC");
- }
- else if( marks > 40 && marks <= 50){
-     console.log("CD");
-  }
-  else if (marks >30 && marks <=40){
-      console.log("DD");
-  }
-   else if ( marks <=30){
-    console.log("FF");
-   }
+//  else if (marks > 80 && marks <= 90){
+//      console.log("AB");
+//  }
+//   else if (marks > 70 && marks <= 80){
+//         console.log("BB");
+//   }
+//  else if( marks > 60 && marks <= 70){
+//          console.log("BC");
+//  }
+//  else if( marks > 50 && marks <= 60){
+//       console.log("CC");
+//  }
+//  else if( marks > 40 && marks <= 50){
+//      console.log("CD");
+//   }
+//   else if (marks >30 && marks <=40){
+//       console.log("DD");
+//   }
+//    else if ( marks <=30){
+//     console.log("FF");
+//    }
 
     
    
    
   
 
-  
-   
+  //Write a JavaScript program that displays the largest integer among two integers.
+
+
+  let a = 3;
+  let b = 3;
+
+  if(a>b){
+    console.log(`the largest number between ${a} and ${b} are := ${a}`)
+  }
+  else if(a<b){
+    console.log(`the largest number between ${a} and ${b} are := ${b}`)
+  }
+  else{
+    console.log(`the largest number between ${a} and ${b} are equal`)
+  }
